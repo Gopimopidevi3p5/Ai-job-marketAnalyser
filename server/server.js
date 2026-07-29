faba24ad82eccd6abb1c365f603089dc9a58032b
gopi mopidevi <ravaagopi3p5@gmail.com>
2026-07-29 13:46:50 +0530

    add verser link in cors

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ const uploaded = multer({
 });
 
 app.use(express.json());
-app.use(cors({ origin: "https://ai-job-market-analyser-rvpj.vercel.app" }));
+app.use(cors({ origin: "https://ai-job-market-analyser-proj.vercel.app" }));
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
