f08f0e4ad71171ed8e3a806ae83d6db948ddf17c
gopi mopidevi <ravaagopi3p5@gmail.com>
2026-07-29 13:11:31 +0530

    vercel link is added to the server side

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ const uploaded = multer({
 });
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "https://ai-job-market-analyser-rvpj.vercel.app/" }));
 
 mongoose
   .connect(process.env.MONGO_URL)
